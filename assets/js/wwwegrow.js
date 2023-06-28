f3360f8159278e7b0f9dfdfab93620bcd53a9eab
wwwegrow <137039446+wwwegrow@users.noreply.github.com>
2023-06-28 16:32:33 +0200

    first commit

---
+++ assets/js/wwwegrow.js
@@ -0,0 +1,47 @@
+// JavaScript code
+document.addEventListener("DOMContentLoaded", function() {
+  var headerSection = document.getElementById("header-section");
+  var dropdownParent = document.querySelector(".dropdown");
+
+  var navLinks = document.getElementsByClassName("nav-link");
+
+  var activeNavLink = null;
+
+  document.addEventListener("click", function(event) {
+    var targetElement = event.target;
+
+    if (targetElement.classList.contains("nav-link")) {
+      var clickedNavLink = targetElement;
+
+      if (clickedNavLink === activeNavLink) {
+        headerSection.classList.remove("active");
+        activeNavLink = null;
+      } else {
+        headerSection.classList.add("active");
+
+        if (activeNavLink) {
+          activeNavLink.classList.remove("active");
+        }
+
+        clickedNavLink.classList.add("active");
+        activeNavLink = clickedNavLink;
+      }
+    } else if (!targetElement.closest(".dropdown-menu")) {
+      headerSection.classList.remove("active");
+
+      if (activeNavLink) {
+        activeNavLink.classList.remove("active");
+        activeNavLink = null;
+      }
+    }
+  });
+
+  dropdownParent.addEventListener("hide.bs.dropdown", function() {
+    headerSection.classList.remove("active");
+
+    if (activeNavLink) {
+      activeNavLink.classList.remove("active");
+      activeNavLink = null;
+    }
+  });
+});
